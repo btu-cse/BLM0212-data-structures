a4b28202441130d9e7e8c2100b375afae1836861
Ahmet Kaşif <ksfahmet@gmail.com>
2021-11-01 15:07:46 +0300

    wk3

--- week3/exp1.c
+++ week3/exp1.c
@@ -1,15 +1,37 @@
-#include <time.h> 
-//C Standart kütüphanesi 
-int main() { 
-    clock_t start, end; 
-    // time.h isimli header dosyasında tanımlı, işlemci saatini saklamak için kullanılan veri türü 
-    double cpu_time_used; 
-    //ölçülen zamanı saniye cinsinden tutmak için tanımlanan değişken 
-    start = clock(); 
-    // ... koşma zamanı ölçülecek kod parçası buraya... 
-    end = clock(); 
-    //Tanımlanmış bir implementasyonun başlangıcından bu yana kullanılan işlemcinin saatini döndürür 
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC; 
-    // CLOCKS_PER_SEC saniyedeki işlemci clock sayısını verir (<time.h> headerında tanımlı) 
-    printf ("%Geçen süre=%.9f saniyedir.", cpu_time_used); 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 200 // 3  10 50  100  200  400 800
+int main()
+{
+    int M1[N][N]={{1,2,3},{4,5,6},{7,8,9}};
+    int M2[N][N]={{1,2,1},{0,0,0},{2,0,1}};
+    int M3[N][N];
+
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+    MultiplyTwoMatrices(M1,M2,N,M3);
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf ("Gecen sure=%.9f saniyedir.", cpu_time_used);
+
+    return 0;
+}
+
+void MultiplyTwoMatrices(int m1[][N], int m2[][N], int n, int m3[][N])
+{
+    int i,j,k,toplam;
+    for (i=0;i<n;i++)
+        for(j=0;j<n;j++){
+            toplam=0;
+            for(k=0;k<n;k++){
+                toplam+=m1[i][k]*m2[k][j];  //Birinci matrisin satirlari ile ikinci matrisin sütunlari carpilir
+            }
+            m3[i][j]=toplam;
+        }
 }
